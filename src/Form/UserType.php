b4c858f55cd731bb9692e7f5fc02ea60dc2005ca
Sophilozoff <sophilozoff@hotmail.fr>
2021-01-24 21:09:15 +0100

    form d'inscription en fr + select genre

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -12,19 +13,57 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('email')
-            ->add('dateOfBirth')
-            ->add('avatar')
-            ->add('description')
+            ->add('username', null, [
+                'label'=>false
+            ])
+            ->add('password', null, [
+                'label'=>false
+            ])
+            ->add('email', null, [
+                'label'=>false
+            ])
+            ->add('sex', ChoiceType::class, [
+                'label'=> false,
+                "choices"=>[
+                    "Féminin"=>"F",
+                    "Masculin"=>"M",
+                    "Non binaire"=>"NB"
+                ],
+                "multiple"=>false,
+            ])
+            ->add('dateOfBirth', null, [
+                'label'=>false
+            ])
+            ->add('avatar', null, [
+                'label'=>false
+            ])
+            ->add('description', null, [
+                'label'=>false
+            ]);
+            if($options['isAdmin']){
+                $builder
+                ->add('roles', ChoiceType::class, [
+                    'label'=>false,
+                    "choices"=>[
+                        "Admin"=>"ROLE_ADMIN",
+                        "Abonné"=>"ROLE_USER"
+                    ],
+                    "multiple"=>true,
+                    "expanded"=>true,
+                    "choice_attr"=>function(){
+                        return["class"=>"form-check-input"];
+                    }
+                ]);
+            };
         ;
     }
+    
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'isAdmin' => false
         ]);
     }
 }
