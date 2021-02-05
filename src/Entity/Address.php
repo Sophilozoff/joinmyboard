fe995cb0d98ab72f96cf0adc8e49e44aac33c67d
Sophilozoff <sophilozoff@hotmail.fr>
2021-02-05 18:15:11 +0100

    ajout bar et address entities

---
+++ src/Entity/Address.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AddressRepository::class)
+ */
+class Address
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $complementStreet;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Bar::class, mappedBy="address", cascade={"persist", "remove"})
+     */
+    private $bar;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(int $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getComplementStreet(): ?string
+    {
+        return $this->complementStreet;
+    }
+
+    public function setComplementStreet(string $complementStreet): self
+    {
+        $this->complementStreet = $complementStreet;
+
+        return $this;
+    }
+
+    public function getBar(): ?Bar
+    {
+        return $this->bar;
+    }
+
+    public function setBar(Bar $bar): self
+    {
+        // set the owning side of the relation if necessary
+        if ($bar->getAddress() !== $this) {
+            $bar->setAddress($this);
+        }
+
+        $this->bar = $bar;
+
+        return $this;
+    }
+}
