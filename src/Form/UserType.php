88c6b964dfce76bd60c12ce1bd4aab507419fee5
Sophilozoff <sophilozoff@hotmail.fr>
2021-02-14 10:34:14 +0100

    repeated password form inscription

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,68 +10,74 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('username', null, [
-                'label'=>false
+                'label' => false
             ])
-            ->add('password', PasswordType::class, [
-                'label'=>false
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
             ])
             ->add('email', null, [
-                'label'=>false
+                'label' => false
             ])
             ->add('roles', ChoiceType::class, [
-                'label'=>false,
-                "choices"=>[
-                    "Admin"=>"ROLE_ADMIN",
-                    "User"=>"ROLE_USER"
+                'label' => false,
+                "choices" => [
+                    "Admin" => "ROLE_ADMIN",
+                    "User" => "ROLE_USER"
                 ],
-                'expanded'=> true,
-                'multiple'=> true,
-                "choice_attr"=>function(){
-                    return["type"=>"radio"];
+                'expanded' => true,
+                'multiple' => true,
+                "choice_attr" => function () {
+                    return ["type" => "radio"];
                 }
             ])
             ->add('sex', ChoiceType::class, [
-                'label'=> false,
-                "choices"=>[
-                    "Féminin"=>"Féminin",
-                    "Masculin"=>"Masculin",
-                    "Non binaire"=>"Non binaire"
+                'label' => false,
+                "choices" => [
+                    "Féminin" => "Féminin",
+                    "Masculin" => "Masculin",
+                    "Non binaire" => "Non binaire"
                 ],
-                "multiple"=>false
+                "multiple" => false
             ])
             ->add('dateOfBirth', null, [
-                'label'=>false
+                'label' => false
             ])
             ->add('avatarFile', VichImageType::class, [
-                'label'=>false
+                'label' => false
             ])
             ->add('description', TextType::class, [
-                'label'=>false
+                'label' => false
             ]);
-            if($options['isAdmin']){
-                $builder
+        if ($options['isAdmin']) {
+            $builder
                 ->add('roles', ChoiceType::class, [
-                    'label'=>false,
-                    "choices"=>[
-                        "Admin"=>"ROLE_ADMIN",
-                        "Abonné"=>"ROLE_USER"
+                    'label' => false,
+                    "choices" => [
+                        "Admin" => "ROLE_ADMIN",
+                        "Abonné" => "ROLE_USER"
                     ],
-                    "multiple"=>true,
-                    "expanded"=>true,
-                    "choice_attr"=>function(){
-                        return["class"=>"form-check-input"];
+                    "multiple" => true,
+                    "expanded" => true,
+                    "choice_attr" => function () {
+                        return ["class" => "form-check-input"];
                     }
                 ]);
-            };
-        ;
+        };;
     }
-    
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
