63d1419102f88fda3fa78db3e058c59ceaf68789
Sophilozoff <sophilozoff@hotmail.fr>
2021-02-15 23:03:02 +0100

    index dashboard mise en forme

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,25 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
- /**
+/**
      * @Route("/admin")
      * @IsGranted("ROLE_ADMIN")
      */
 class AdminController extends AbstractController
 {
+
+
     /**
      * @Route("/accueil", name="admin_index")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository,
+    EventRepository $eventRepository): Response
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'countAllUsers' => $userRepository->countById(),
+            'countAllEvents' => $eventRepository->countById()
         ]);
     }
 }
